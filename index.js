17dac18056aa0b3f3899c2465ce83c63993f1478
anlsanchez@ucdavis.edu <anlsanchez@ucdavis.edu>
2018-04-03 18:50:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,15 +16,14 @@ function logWhisper(string){
   console.log(little)
 }
 
-function sayHiToGrandma(string)
-{
-  if (string = string.toLowerCase()) {
+function sayHiToGrandma(string) {
+  if (string === string.toLowerCase()) {
  response = "I can\'t hear you!";
   }
-  else if (string = string.toUpperCase()) {
+  else if (string === string.toUpperCase()) {
   response = "YES INDEED!";
   }
-   else if (string = "I love you, Grandma.") {
+   else if (string === "I love you, Grandma.") {
   response = "I love you, too.";
   }
   else {}
